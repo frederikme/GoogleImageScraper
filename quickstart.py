baa34c4de4b5981eeb39422d2f74e80fd82bd580
Frederik <60892381+frederikme@users.noreply.github.com>
2020-10-07 21:03:11 +0200

    Create quickstart.py

---
+++ quickstart.py
@@ -0,0 +1,6 @@
+from scraper.py import GoogleImageScraper
+
+if __name__ == '__main__':
+    scraper = GoogleImageScraper()
+    # By default, directory='images' and amount='50'
+    scraper.downloadImages(query="Goedele Liekens", directory='testimages', amount=30)
